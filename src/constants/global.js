91dd738705db0a4e78d26b55b9e5b21da9d97356
Alex Canales <alexcanales766@gmail.com>
2022-09-08 22:06:24 -0400

    Created utils and constants for the project

---
+++ src/constants/global.js
@@ -0,0 +1,10 @@
+export const APP_CONTAINER_DIV_ID = 'mvp-app';
+export const DEFAULT_GROUP_ID = 'GroupCompanyId';
+export const DEFAULT_LANGUAGE = 'en';
+export const NUMBER_CLASS = 'number';
+
+export const DEFAULT_DATABASE_VERSION = 2100;
+export const DEFAULT_TIMEZONE = 'America/New_York';
+export const USER_DENIED_ACCESS_ERROR = 'user does not have access to app';
+export const LOCK_IMAGE_PATH = '/images/lock.svg';
+export const ERROR_SIGN_IMAGE_PATH = '/images/error-sign.svg';
---
+++ src/utils/fetch-time.js
@@ -0,0 +1,25 @@
+export const getCurrentDateTime = () => {
+  const currentDate = new Date();
+  const currentDateTime = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()} ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
+
+  return currentDateTime;
+};
+
+export const getCurrentDateInYYYYMMDD = () => {
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+  const yyyy = today.getFullYear();
+  return `${yyyy}-${mm}-${dd}`;
+};
+
+export const getCurrentDate = () => {
+  const currentDate = new Date();
+  return currentDate;
+};
+
+export const getCurrentTime = () => {
+  const currentDate = new Date();
+  const currentTime = `${String(currentDate.getHours()).padStart(2, '0')}:${String(currentDate.getMinutes()).padStart(2, '0')}`;
+  return currentTime;
+};
