ea2a6fb743527955e2edfce5198b362e9effe6e8
canaleal <alexcanales766@gmail.com>
2022-11-14 03:16:00 -0500

    Added icons for splash screen

--- src/screens/SplashScreen.jsx
+++ src/screens/SplashScreen.jsx
@@ -1,14 +1,15 @@
+import React from 'react';
+import { View, Image } from 'react-native';
 
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import GlobalStyles from '../styles/global-styles';
 
 function SplashScreen() {
-    return (
-      <View>
-        <Text>Loading...</Text>
-      </View>
-    );
-  
-    }
+  return (
+    <View style={GlobalStyles.flex1}>
+      <Image source={require('../../assets/AppLogoBlue.png')} />
+    </View>
+  );
 
-    export default SplashScreen;
+}
+
+export default SplashScreen;
